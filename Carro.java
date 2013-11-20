25c61dc86d1e4b764e6d2285dd7f05e487c3c17f
desenwebcf <desenwebcf@yahoo.com.br>
2013-11-19 22:04:27 -0200

    Create Carro.java

---
+++ Carro.java
@@ -0,0 +1,88 @@
+
+package modelo;
+import java.util.Vector;
+//import persistencia.DAO.ClienteDAO;
+
+
+public class Carro {
+    private String placa;
+    private String cor;
+    private String modelo;
+    private String fabricante;
+    private int renavan;
+
+    
+    /**
+     * @return the placa
+     */
+    public String getPlaca() {
+        return placa;
+    }
+
+    /**
+     * @param placa the placa to set
+     */
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    /**
+     * @return the cor
+     */
+    public String getCor() {
+        return cor;
+    }
+
+    /**
+     * @param cor the cor to set
+     */
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    /**
+     * @return the modelo
+     */
+    public String getModelo() {
+        return modelo;
+    }
+
+    /**
+     * @param modelo the modelo to set
+     */
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    /**
+     * @return the fabricante
+     */
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    /**
+     * @param fabricante the fabricante to set
+     */
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    /**
+     * @return the renavan
+     */
+    public int getRenavan() {
+        return renavan;
+    }
+
+    /**
+     * @param renavan the renavan to set
+     */
+    public void setRenavan(int renavan) {
+        this.renavan = renavan;
+    }
+    
+    
+     
+    
+}
